83a0ca72f0ff5f42df15e4439cf5b9cff41d2b43
satyatorati <thoratisunny@gmail.com>
2025-03-21 18:59:22 -0400

    Fix path to frontend dist folder

--- backend/src/server.js
+++ backend/src/server.js
@@ -43,10 +43,11 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 3000;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../../frontend/dist", "index.html"));
   });
 }
 
